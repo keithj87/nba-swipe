3fc7e2fed8e112ed596ce5067b8a5c06fd1900d1
Anuvind Menon <manuvind@gmail.com>
2019-02-09 16:04:25 -0500

    randomize

--- app/apiEndpoint.py
+++ app/apiEndpoint.py
@@ -51,12 +51,14 @@ def fetch_matchups():
             query["Year"] = {"$in": ['2000', ' 2001', ' 2002', ' 2003', ' 2004', ' 2005', ' 2006', ' 2007', ' 2008', ' 2009', ' 2010', ' 2011', ' 2012', ' 2013', ' 2014', ' 2015', ' 2016', ' 2017']}
             print(query)
             # Fetch all the record(s)
-            records_fetched = collection.find(query)
-
+            records_fetched = list(collection.find(query))
+            #pdb.set_trace()
             # Check if the records are found
-            if records_fetched.count() >= 2:
+            if len(records_fetched) >= 2:
+                #pdb.set_trace()
                 print("hello world")
                 output = []
+                output.append(random.choice(records_fetched))
                 output.append(random.choice(records_fetched))
                 # Prepare the response
                 return dumps(output)
